56c719303e7ab84ca5c718c81c305d5d8e8a1d27
MattW <matt.werth@fivebyfiveglobal.com>
2022-05-30 21:02:44 +1000

    dots working

--- wp-content/themes/launchcore-child/src/js/blocks/slider.js
+++ wp-content/themes/launchcore-child/src/js/blocks/slider.js
@@ -4,10 +4,22 @@ $(document).ready(function(){
 	$(document).find('.b-slider').each(function(){
 		let thisSlider = $(this);
 		let mySlides = thisSlider.find('.b-slide');
-
+		let mySlidesCount = mySlides.length;
+		
+		// fill the dots
+		let myDots = thisSlider.find('[data-slider-dots]');
+		let dotCode = "<div class='b-slider__dot'></div>".repeat(mySlidesCount);
+		myDots.html(dotCode);
 		// reveal the first slide
 		$(mySlides[0]).addClass('is-current');
 
+		//give each slide an index
+		mySlides.each(function(index){
+			$(this).attr('data-index', index);
+		});
+
+		// fill in the appropriate dot
+		dotCurrent();
 	})
 
 	$(document).on('click touchend', '[data-slider-next]', function(e){
@@ -26,7 +38,7 @@ $(document).ready(function(){
 			$(mySlider.find('.b-slide')[0]).addClass('is-current');
 		};
 		currentSlide.removeClass('is-current');
-
+		dotCurrent();
 	})
 
 	$(document).on('click touchend', '[data-slider-prev]', function(e){
@@ -47,7 +59,18 @@ $(document).ready(function(){
 			$(mySlides[mySlidesCount - 1]).addClass('is-current');
 		};
 		currentSlide.removeClass('is-current');
-
+		dotCurrent();
 	})
+
+	function dotCurrent(){
+		// do for each slider
+		$(document).find('.b-slider').each(function(){
+			let thisSlider = $(this);
+			let currentIndex = thisSlider.find('.is-current').attr('data-index');
+			let dots = $(document).find(".b-slider__dot");
+			dots.removeClass('is-current');
+			$(dots[currentIndex]).addClass('is-current');
+		});
+	}
 
 })
