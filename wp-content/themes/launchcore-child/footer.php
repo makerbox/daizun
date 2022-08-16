c6929722b6a3abdbc90a8ff2dc5cb442bbc1715e
MattW <matt.werth@fivebyfiveglobal.com>
2022-08-16 18:04:05 +1000

    cowboy

--- wp-content/themes/launchcore-child/footer.php
+++ wp-content/themes/launchcore-child/footer.php
@@ -8,7 +8,15 @@
 						53 Davies Street<br/>
 						Mayfair<br/>
 						London<br/>
-						W1K 5JH
+						W1K 5JH<br/>
+						<div style="display: inline-block; width: 100%; max-width: 1140px;">
+							<a href="https://nacfb.org" target="_blank" style="width: 5rem; display: inline-block; padding: .5rem; float: left;">
+								<img src="https://daizun.co.uk/wp-content/uploads/2022/08/NACFB_Logo_with_strapline_RGB-1.png" class="wp-image-317">
+							</a>
+							<a  href="https://propertyinvestor.co.uk/attend/register/?promoid=YOUR-PROMO-CODE" target="_blank" style="width: 5rem; display: inline-block; padding: .5rem; float: right;">
+								<img src="https://daizun.co.uk/wp-content/uploads/2022/08/Website-Banner-3.jpeg" class="wp-image-316">
+							</a>
+						</div>
 					</div>
 					<div class="p-footer__legal">
 						Daizun Investments is a provider of non-regulated loans against properties that are registered in England and Wales. All Offers of Finance are subject to satisfactory legal due diligence, a valuation report, ‘know your client checks’ and, for Development Finance only, the initial monitoring surveyors’ reports.
